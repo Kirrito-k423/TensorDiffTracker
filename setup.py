4aeb0b9d2eabecc1c0fd5dc5f25bae0f7ed6f702
shaojieMike <943648187@qq.com>
2025-03-19 15:10:50 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,10 @@ setup(
     name="ttracker",
     version="0.1.0",
     py_modules=["ttracker"],  # 指定单个文件作为模块
+    install_requires=[
+        "torch>=2.0.0",        # 需要GPU支持时添加版本约束
+        "numpy>=1.21.0",
+        "icecream>=2.1.3",
+        # 其他隐式依赖（如系统自带库）无需声明
+    ],
 )
